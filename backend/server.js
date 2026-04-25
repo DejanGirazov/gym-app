b196b0672d970a733a8fe8f2346ae1a7a7c93c57
Dejan <dgirazov@gmail.com>
2026-04-25 19:08:59 +0200

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import path from 'path'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 import connectDB from './MongoDB/connectDB.js'
@@ -10,6 +11,7 @@ dotenv.config()
 
 
 const app = express();
+const __dirname= path.resolve();
 
 app.use(express.json());
 app.use(cookieParser()) 
@@ -19,6 +21,13 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/workout', workoutRoutes);
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')))
+
+    app.get('/*splat', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../frontend/dist/index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 5000
 
