86589ee59df44da6b04e18a05701c16ccca936db
Dejan <dgirazov@gmail.com>
2026-06-10 17:14:15 +0200

    update mealController with search endpoint

--- backend/routes/mealRoutes.js
+++ backend/routes/mealRoutes.js
@@ -1,19 +1,21 @@
 import express from "express";
 import {
     createLog,
-    getAllMeals,
-    updateMeal,
     deleteMeal,
-    getMeal
+    getAllMeals,
+    getMeal,
+    searchMeal,
+    updateMeal,
 } from "../controllers/mealController.js";
 import { protectRoute } from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
-router.post("/create",protectRoute, createLog);
+router.post("/create", protectRoute, createLog);
 router.get("/getLogs", protectRoute, getAllMeals);
 router.get("/getLog/:id", protectRoute, getMeal);
 router.put("/update/:id", protectRoute, updateMeal);
 router.delete("/delete/:id", protectRoute, deleteMeal);
+router.get("/search", protectRoute, searchMeal);
 
 export default router;
